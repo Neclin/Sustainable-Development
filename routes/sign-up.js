92f79c6fae78dae7de96f33445d1d96534a9920a
Neclin <chapmanluke05@icloud.com>
2023-12-03 13:24:00 +0000

    Fixed signup url so its consistent on all pages

--- routes/sign-up.js
+++ routes/sign-up.js
@@ -3,11 +3,7 @@ const router = express.Router();
 const signupJSON = require("../views/sign-up/sign-up.json");
 
 router.get("/", (req, res) => {
-  res.send("Newsletter Signup");
-});
-
-router.get("/sign-up", (req, res) => {
-    res.render("sign-up/sign-up", signupJSON);
+  res.render("sign-up/sign-up", signupJSON);
 });
 
 module.exports = router;
