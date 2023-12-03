b580ded0948b85a13cbe822067c47a5414e7f35b
neclin <chapmanluke05@icloud.com>
2023-12-03 22:37:13 +0000

    Added media queries to the goals pages

--- public/goals.js
+++ public/goals.js
@@ -1,17 +1,41 @@
 let slideIndex = 1;
+let numberOfSlides = 1;
 showSlides(slideIndex);
 
 function changeSlides(offset) {
   showSlides((slideIndex += offset));
 }
 
-function showSlides(n) {
+function showSlides(start) {
   const slides = document.getElementsByClassName("slide");
-  n > slides.length ? (slideIndex = 1) : null;
-  n < 1 ? (slideIndex = slides.length) : null;
+  if (slideIndex < 1) {
+    slideIndex = 1;
+  }
+  if (slideIndex + numberOfSlides - 1 > slides.length) {
+    slideIndex = slides.length - numberOfSlides + 1;
+  }
+  console.log(slideIndex);
+  // Get all slides and hide them
   for (let i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  slides[slideIndex - 1].style.display = "flex";
-  slides[slideIndex].style.display = "flex";
+
+  for (let i = slideIndex - 1; i < slideIndex + numberOfSlides - 1; i++) {
+    slides[i].style.display = "block";
+  }
 }
+
+const widthQuery = window.matchMedia("(max-width: 800px)");
+
+function checkWidth(widthQuery) {
+  if (widthQuery.matches) {
+    numberOfSlides = 1;
+  } else {
+    numberOfSlides = 2;
+  }
+  showSlides(slideIndex);
+}
+
+widthQuery.addEventListener("change", checkWidth);
+
+checkWidth(widthQuery);
