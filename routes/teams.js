0d3dc7fac60a3fd9cf2c6724307fce70fcf23e15
neclin <chapmanluke05@icloud.com>
2023-12-08 00:49:16 +0000

    Team page redesign and goal 4 content

--- routes/teams.js
+++ routes/teams.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const signupJSON = require("../views/teams/teams.json");
+const signupJSON = require("../views/teams/teamsData.json");
 
 router.get("/", (req, res) => {
   res.render("teams/teams", signupJSON);
