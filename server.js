24ea57adfb424904148d130b934fab5f6713dd38
Neclin <chapmanluke05@icloud.com>
2023-12-10 02:12:30 +0000

    Added email confirmation

--- server.js
+++ server.js
@@ -5,7 +5,19 @@ const port = 3000;
 const bodyParser = require("body-parser");
 const jsonParser = bodyParser.json();
 
-const fs = require('fs'); // 
+const fs = require("fs"); //
+
+const nodemailer = require("nodemailer");
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    // I cant Make an email apparently So its named wrong
+    user: "groupY23dev@gmail.com",
+    pass: "zrkb dsss wrbb qgea",
+  },
+});
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
@@ -29,23 +41,40 @@ app.listen(port, () => {
 });
 
 app.post("/form", jsonParser, (req, res) => {
-  fs.readFile("./formDetails.JSON", (err, data) => {  // reads all the data in the JSON file and asigns it with the data parameter
+  fs.readFile("./formDetails.JSON", (err, data) => {
+    // reads all the data in the JSON file and asigns it with the data parameter
     if (err) {
-      console.log("Failed to read file.")
+      console.log("Failed to read file.");
       return;
     }
 
-    const newData = JSON.parse(data)
-    newData.entries.push(req.body) // appends the new users entry to all the existing entries within the file
+    const newData = JSON.parse(data);
+    newData.entries.push(req.body); // appends the new users entry to all the existing entries within the file
 
-    fs.writeFile("./formDetails.JSON", JSON.stringify(newData), (error) => {  // converts the combined old entries and new entries back into json objects and writes it to the file
-      if (error){
-        console.log("Entry could not be added.")
+    fs.writeFile("./formDetails.JSON", JSON.stringify(newData), (error) => {
+      // converts the combined old entries and new entries back into json objects and writes it to the file
+      if (error) {
+        console.log("Entry could not be added.");
         return;
       }
-      console.log("Entry was successfully added.")
-    })
+      console.log("Entry was successfully added.");
+    });
+  });
+  res.send(`Thank you ${req.body.firstName} for joining our newsletter!`);
 
-  })
-  res.send(`Thank you ${req.body.firstName} for joining our newsletter!`)
+  const mailOptions = {
+    from: "Group Y22 ",
+    to: req.body.email,
+    subject: "Thank you for signing up to our newsletter!",
+    text: `Thank you ${req.body.firstName} for joining our newsletter!`,
+  };
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+      return;
+    } else {
+      console.log("Email sent: " + info.response);
+    }
+  });
 });
