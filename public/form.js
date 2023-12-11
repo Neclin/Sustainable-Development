e7e590877d6b59057df1317f01bdcfff28ad4257
Neclin <chapmanluke05@icloud.com>
2023-12-11 00:23:11 +0000

    FIxed multiple confirmations messages on sigup pages

--- public/form.js
+++ public/form.js
@@ -1,45 +1,49 @@
-function onResponse(response){
-    return response.text();
+function onResponse(response) {
+  return response.text();
 }
 
-function onResultReady(text){
-    const confirmationDiv = document.querySelector("#entryConfirmation");
-    const confirmationText = document.createElement("p");
-    confirmationText.innerHTML = text;
-    confirmationText.id = "entryConfirmText"
-    confirmationDiv.appendChild(confirmationText);
+function onResultReady(text) {
+  const confirmationDiv = document.querySelector("#entryConfirmation");
+  // Remove any previous confirmation text
+  if (confirmationDiv.hasChildNodes()) {
+    confirmationDiv.removeChild(confirmationDiv.childNodes[0]);
+  }
+  const confirmationText = document.createElement("p");
+  confirmationText.innerHTML = text;
+  confirmationText.id = "entryConfirmText";
+  confirmationDiv.appendChild(confirmationText);
 }
 
-function processForm(e){
-    e.preventDefault();
+function processForm(e) {
+  e.preventDefault();
 
-    const userFirstName = document.querySelector("#firstName").value;
-    const userLastName = document.querySelector("#lastName").value;
-    const userEmail = document.querySelector("#email").value;
-    const userComment = document.querySelector("#comments").value;
+  const userFirstName = document.querySelector("#firstName").value;
+  const userLastName = document.querySelector("#lastName").value;
+  const userEmail = document.querySelector("#email").value;
+  const userComment = document.querySelector("#comments").value;
 
-    const formResult = {
-        firstName: userFirstName,
-        lastName: userLastName,
-        email: userEmail,
-        comment: userComment
-    }
+  const formResult = {
+    firstName: userFirstName,
+    lastName: userLastName,
+    email: userEmail,
+    comment: userComment,
+  };
 
-    const formResultJSON = JSON.stringify(formResult);
+  const formResultJSON = JSON.stringify(formResult);
 
-    const fetchFormResult = {
-        method: "POST",
-        headers:{
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: formResultJSON
-    }
+  const fetchFormResult = {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: formResultJSON,
+  };
 
-    fetch("http://localhost:3000/form", fetchFormResult)
+  fetch("http://localhost:3000/form", fetchFormResult)
     .then(onResponse)
     .then(onResultReady);
 }
 
 const form = document.querySelector("#form");
-form.addEventListener('submit', processForm)
+form.addEventListener("submit", processForm);
